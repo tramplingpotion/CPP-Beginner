55edfcac83ffc3d0945d7ef1132b904b1f46ac0f
tramplingpotion <syamilfahman7007@gmail.com>
2022-09-25 11:08:19 +0800

    code cleanup

--- CPP-Array/array.cpp
+++ CPP-Array/array.cpp
@@ -1,5 +1,5 @@
-// alphabet[2] = {"s","d"} 
-// array alphabet has array size of 2 
+// alphabet[2] = {"s","d"}
+// array alphabet has array size of 2
 // s is at "index" 0 while d is at index 1
 // the "element" of index 0 is "s" while index 1 is "d"
 
@@ -14,7 +14,7 @@ int main(int argc, char const *argv[])
 
     std::cout << cars[2] << std::endl;
     std::cout << num[2];
-    
+
     std::cin.get();
     return 0;
 }
--- CPP-Loops/for.cpp
+++ CPP-Loops/for.cpp
@@ -1,12 +1,12 @@
-//use it when know how many times you want to loop through a block of code
-// step-init,condition,plus
+// use it when know how many times you want to loop through a block of code
+//  step-init,condition,plus
 #include <iostream>
 
 int main(int argc, char const *argv[])
 {
-    for(int i=1;i<=10;i++)
+    for (int i = 1; i <= 10; i++)
     {
-        std::cout<<"For Loop "<<i<<"# Times."<<std::endl;
+        std::cout << "For Loop " << i << "# Times." << std::endl;
     }
     std::cin.get();
     return 0;
