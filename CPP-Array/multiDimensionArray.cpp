c75876ae3df95b2b2b00174593ae09f9d29a97ef
tramplingpotion <syamilfahman7007@gmail.com>
2022-10-03 05:49:34 +0800

    3 dimensional array

--- CPP-Array/multiDimensionArray.cpp
+++ CPP-Array/multiDimensionArray.cpp
@@ -16,5 +16,33 @@ int main(int argc, char const *argv[])
         }
     }
 
+    std::cout<<"\n";
+
+    //3 dimensional array
+    std::string letters1[2][2][2]={
+        {
+            {"a","b"},
+            {"c","d"}
+        },
+        {
+            {"e","f"},
+            {"g","h"}
+        }
+    };
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            for (int k  = 0; k < 2; k++)
+            {
+                std::cout<<letters1[i][j][k]<<std::endl;
+            }
+            
+        }
+        
+    }
+    
+
     return 0;
 }
