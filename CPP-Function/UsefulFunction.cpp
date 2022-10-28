0472b67a5d93db7f1bf8c52b0dbe166b0aab061b
tramplingpotion <syamilfahman7007@gmail.com>
2022-10-29 03:39:37 +0800

    Delete UsefulFunction.cpp

--- CPP-Function/UsefulFunction.cpp
+++
@@ -1,8 +0,0 @@
-
-//get array size function
-//not understand why if using the formula through function the output is different to just using it in variable.
-int getArraySize(int arrayVar[])
-{
-    int arraySize = sizeof(arrayVar) / sizeof(int);
-    return arraySize;
-}
