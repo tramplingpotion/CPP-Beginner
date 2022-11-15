9da950252a6a8e29c7d822cfb702665e801d6d47
tramplingpotion <syamilfahman7007@gmail.com>
2022-11-16 00:12:48 +0800

    tidy file and syntax

--- CPP-Reference/reference.cpp
+++ CPP-Reference/reference.cpp
@@ -1,4 +1,4 @@
-//learn pointers first
+// learn pointers first
 
 // reference is referencing to existing variable
 // which mean that we use reference to get/use the value of the variable (using the same memory address)
@@ -10,11 +10,11 @@
 int main(int argc, char const *argv[])
 {
     std::string meal = "pizza";
-    std::string &food = meal;//using the same method as pointer to get the memory address
+    std::string &food = meal; // using the same method as pointer to get the memory address
     // string &food
     //  same method of use
 
-    std::cout << meal << " & " << food;//notice that here food doesnt have & because it is the method to get memory address in pointer
+    std::cout << meal << " & " << food; // notice that here food doesnt have & because it is the method to get memory address in pointer
 
     return 0;
 }
