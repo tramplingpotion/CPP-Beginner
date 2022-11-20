a7cae6a3d0691d7c0d5c92278f2f099ee2da4aff
tramplingpotion <syamilfahman7007@gmail.com>
2022-11-20 23:51:17 +0800

    Update defaultFunctParam.cpp

--- CPP-Function/defaultFunctParam.cpp
+++ CPP-Function/defaultFunctParam.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 // default parameter will display parameter initial value
+// void function generally used displaying in console and setting the value of variable
 void myCountry(std::string country = "Singapore")
 {
     std::cout<<"Country of "<<country<<std::endl;
